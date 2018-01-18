4c79b0eaede1d7de4f3bdafa4ca2dc45cf7567fb
Dmitry Suvorov <soovorow@gmail.com>
2018-01-18 14:25:47 +0300

    add social features

--- js/custom.js
+++ js/custom.js
@@ -1,33 +1,31 @@
-function generateWhatWeNeed() {
+(function(yourcode) {
+    yourcode(window.jQuery, window, document);
+}(function($, window, document) {
 
     var wordListOne = ["24/7", "multi-Tier", "30,000 foot", "B-to-B", "win-win", "front-end", "web-based", "pervasive", "smart", "six-sigma", "critical-path", "dynamic"],
         wordListTwo = ["empowerd", "sticky", "value-added", "oriented", "centric", "distributed", "clustered", "branded", "outside-the-box", "positioned", "networked", "focused", "leveraged", "aligned", "targeted", "shared", "cooperative", "accelerated"],
         wordListThree = ["process", "tripping-point", "solution", "architecture", "core competency", "strategy", "mindshare", "portal", "space", "vision", "paradigm", "mission"];
 
-    var oneLength = wordListOne.length,
-        twoLength = wordListTwo.length,
-        threeLength = wordListThree.length;
-
-    var rand1 = parseInt(Math.random() * oneLength),
-        rand2 = parseInt(Math.random() * twoLength),
-        rand3 = parseInt(Math.random() * threeLength);
-
-    console.log(rand1);
-
-    return "a " + wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
-}
-
-(function(yourcode) {
-    yourcode(window.jQuery, window, document);
-}(function($, window, document) {
     $(function() {
 
         var body = $('body'),
             button = $('#button'),
-            result = $('#result');
+            result = $('#result'),
+            tweet_button = $('#tweet');
 
         var action_body_class = 'cast',
             action_button_class = 'casting';
+
+        if (window.location.search.length > 0) {
+            var str = window.location.search,
+                keys = str.split('=')[1].split('+'),
+                phrase = "a " + wordListOne[keys[0]] + " " + wordListTwo[keys[1]] + " " + wordListThree[keys[2]];
+
+            result.html(phrase);
+            button.html('[ask again]');
+
+            updateTweetButton(phrase, str);
+        }
 
         body.on('click', '#button', function () {
 
@@ -38,14 +36,38 @@ function generateWhatWeNeed() {
             body.addClass(action_body_class);
             button.html('').addClass(action_button_class);
             result.html('...');
+            tweet_button.hide();
 
             setTimeout(function () {
+                phrase = generateWhatWeNeed();
                 body.removeClass(action_body_class);
-                result.html(generateWhatWeNeed());
+                result.html(phrase);
                 button.html('[ask again]').removeClass(action_button_class);
+                updateTweetButton(phrase, window.location.search);
             }, 3000)
-        })
+        });
 
+        function updateTweetButton(phrase, params) {
+            tweet_button.show();
+            tweet_button.attr('href', 'https://twitter.com/intent/tweet?text=What we need is a ' + phrase + ': http://phrase-o-matic.soovorow.com/' + params)
+        }
     });
+
+    function generateWhatWeNeed() {
+        var oneLength = wordListOne.length,
+            twoLength = wordListTwo.length,
+            threeLength = wordListThree.length;
+
+        var rand1 = parseInt(Math.random() * oneLength),
+            rand2 = parseInt(Math.random() * twoLength),
+            rand3 = parseInt(Math.random() * threeLength);
+
+        window.history.pushState(null, null, "/?share=" + rand1 + '+' + rand2 + '+' + rand3);
+
+        return "a " + wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+    }
+
+
+
 }));
 
